20a12a442b996c193ae33895ee96c5e3870256a7
uncenter <uncenter@uncenter.dev>
2025-07-22 22:57:37 -0400

    style: consistently use tab indentation, trailing newlines

--- 4/.eleventy.js
+++ 4/.eleventy.js
@@ -1,8 +1,8 @@
 module.exports = (config) => {
 	return {
-        markdownTemplateEngine: 'njk',
-        dataTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',
+		markdownTemplateEngine: 'njk',
+		dataTemplateEngine: 'njk',
+		htmlTemplateEngine: 'njk',
 		dir: {
 			input: 'src',
 			output: 'dist',
