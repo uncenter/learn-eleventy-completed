2a5893ea923df40c29a4227e2a46858a375e0251
uncenter <uncenter@uncenter.dev>
2025-12-29 22:50:35 -0500

    feat: lesson 8

--- eleventy-from-scratch/eleventy.config.js
+++ eleventy-from-scratch/eleventy.config.js
@@ -4,9 +4,32 @@ export default function (eleventyConfig) {
 
 	// Set directories to pass through to the dist folder
 	eleventyConfig.addPassthroughCopy('src/images');
+
+	// Returns work items, sorted by display order
+	eleventyConfig.addCollection('work', (collection) => {
+		return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+	});
+	// Creates and returns a collection of work that is set to be featured
+	eleventyConfig.addCollection('featuredWork', (collection) => {
+		return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+			(x) => x.data.featured,
+		);
+	});
 }
 
 export const config = {
 	markdownTemplateEngine: 'njk',
 	htmlTemplateEngine: 'njk',
 };
+
+/**
+ * Takes a collection and returns it back in display order
+ *
+ * @param {Array} collection The 11ty collection
+ * @returns {Array} the sorted collection
+ */
+function sortByDisplayOrder(collection) {
+	return collection.sort((a, b) =>
+		Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1,
+	);
+}
