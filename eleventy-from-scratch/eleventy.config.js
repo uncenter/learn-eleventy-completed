71657216355e0821528ce2adf8472ff78ec7d7fc
uncenter <uncenter@uncenter.dev>
2025-12-29 22:39:52 -0500

    feat: lesson 5

--- eleventy-from-scratch/eleventy.config.js
+++ eleventy-from-scratch/eleventy.config.js
@@ -1,6 +1,9 @@
 export default function (eleventyConfig) {
 	eleventyConfig.setInputDirectory('src');
 	eleventyConfig.setOutputDirectory('dist');
+
+	// Set directories to pass through to the dist folder
+	eleventyConfig.addPassthroughCopy('src/images');
 }
 
 export const config = {
