d87e0afb2a01ea0ba726a8982f40049caa92fe65
uncenter <uncenter@uncenter.dev>
2025-12-29 23:18:00 -0500

    feat: lesson 13

--- eleventy-from-scratch/src/_data/helpers.js
+++ eleventy-from-scratch/src/_data/helpers.js
@@ -20,3 +20,42 @@ export function getLinkActiveState(itemUrl, pageUrl) {
 
 	return response;
 }
+
+/**
+ * Filters out the passed item from the passed collection
+ * and randomizes and limits them based on flags
+ *
+ * @param {Array} collection The 11ty collection we want to take from
+ * @param {Object} item The item we want to exclude (often current page)
+ * @param {Number} limit=3 How many items we want back
+ * @param {Boolean} random=true Whether or not this should be randomized
+ * @returns {Array} The resulting collection
+ */
+
+export function getSiblingContent(collection, item, limit = 3, random = true) {
+	let filteredItems = collection.filter(x => x.url !== item.url);
+
+	if (random) {
+		let counter = filteredItems.length;
+
+		while (counter > 0) {
+			// Pick a random index
+			let index = Math.floor(Math.random() * counter);
+
+			counter--;
+
+			let temp = filteredItems[counter];
+
+			// Swap the last element with the random one
+			filteredItems[counter] = filteredItems[index];
+			filteredItems[index] = temp;
+		}
+	}
+
+	// Lastly, trim to length
+	if (limit > 0) {
+		filteredItems = filteredItems.slice(0, limit);
+	}
+
+	return filteredItems;
+}
