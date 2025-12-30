92c0387a10b44bf8da246504ce8c07c981448ff5
uncenter <uncenter@uncenter.dev>
2025-12-30 00:06:05 -0500

    feat: lesson 19

--- eleventy-from-scratch/eleventy.config.js
+++ eleventy-from-scratch/eleventy.config.js
@@ -18,6 +18,7 @@ export default function (eleventyConfig) {
 
 	// Set directories to pass through to the dist folder
 	eleventyConfig.addPassthroughCopy('src/images');
+	eleventyConfig.addPassthroughCopy('src/fonts');
 
 	// Plugins
 	eleventyConfig.addPlugin(rssPlugin);
